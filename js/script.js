5c7c1d2e28f4f3c4e8d5282ab2487a47bf7e31fe
Harrison Ly <harriko@gmail.com>
2016-10-18 23:16:13 +0100

    changed resolution null

--- js/script.js
+++ js/script.js
@@ -5,14 +5,15 @@ $(document).ready(function() {
 function createGrid(resolution) {
   var containerGrid = $('#container');
   if(resolution == null) {
-    var pixelSize = 600 / 16;
-    for (var c = 0; c < 16; c++) {
-      for (var r = 0; r < 16; r++) {
-        containerGrid.append('<div class="grid"></div>');
-        $('.grid').css({'width': pixelSize + 'px', 'height': pixelSize + 'px'});
-      }
-      //containerGrid.append('<br>');
+    resolution = 16;
+  }
+  var pixelSize = 600 / resolution;
+  for (var c = 0; c < resolution; c++) {
+    for (var r = 0; r < resolution; r++) {
+      containerGrid.append('<div class="grid"></div>');
+      $('.grid').css({'width': pixelSize + 'px', 'height': pixelSize + 'px'});
     }
+    //containerGrid.append('<br>');
   }
 }
 function draw() {
