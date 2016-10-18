04da0d86d56aa7015951121fd1d8bb4d33691796
Harrison Ly <harriko@gmail.com>
2016-10-18 23:31:00 +0100

    faster grid creation

--- js/script.js
+++ js/script.js
@@ -5,15 +5,13 @@ $(document).ready(function() {
 function createGrid(resolution) {
   var containerGrid = $('#container');
   if(resolution == null) {
-    resolution = 16;
+    resolution = 64;
   }
   var pixelSize = 600 / resolution;
   for (var c = 0; c < resolution; c++) {
     for (var r = 0; r < resolution; r++) {
-      containerGrid.append('<div class="grid"></div>');
-      $('.grid').css({'width': pixelSize + 'px', 'height': pixelSize + 'px'});
+      containerGrid.append('<div class="grid" style="width: ' + pixelSize + 'px; height: ' + pixelSize + 'px;"></div>');
     }
-    //containerGrid.append('<br>');
   }
 }
 function draw() {
