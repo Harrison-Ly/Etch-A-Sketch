4c9b7aaf4c729286085031c060b7be8cfc9d55b5
Harrison Ly <harriko@gmail.com>
2016-10-18 23:06:56 +0100

    added grid and you can draw with your mouse

--- js/script.js
+++ js/script.js
@@ -1,3 +1,22 @@
 $(document).ready(function() {
-  
+  createGrid();
+  draw();
 })
+function createGrid(resolution) {
+  var containerGrid = $('#container');
+  if(resolution == null) {
+    var pixelSize = 600 / 16;
+    for (var c = 0; c < 16; c++) {
+      for (var r = 0; r < 16; r++) {
+        containerGrid.append('<div class="grid"></div>');
+        $('.grid').css({'width': pixelSize + 'px', 'height': pixelSize + 'px'});
+      }
+      //containerGrid.append('<br>');
+    }
+  }
+}
+function draw() {
+  $('.grid').on('mouseenter', function(){
+    $(this).addClass('gridBlack');
+  });
+}
